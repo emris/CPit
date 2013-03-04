34dc06ab64d182e7c55ca7867df91f97b84f7c43
emris <emris@pc-net.at>
2013-03-04 14:38:04 +0100

    Works also in Singleplayer now

--- src/emris/mod/CPit/CPit.java
+++ src/emris/mod/CPit/CPit.java
@@ -1,30 +1,20 @@
 package emris.mods.CPit;
 
-import net.minecraft.command.ICommandManager;
-import net.minecraft.command.ServerCommandManager;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.src.ModLoader;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.Mod.ServerStarting;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid="CPit", name="Charcoal Pit", version="1.2.b75")
+@Mod(modid="CPit", name="Charcoal Pit", version="1.3.b75")
 @NetworkMod(clientSideRequired=false, serverSideRequired=false)
 public class CPit {
 
 	@Instance("CPit")
 	public static CPit instance;
 	
-	@Init
-	public void load(FMLInitializationEvent event) {
-		if (event.getSide() == Side.SERVER) {
-			MinecraftServer server = ModLoader.getMinecraftServerInstance();
-			ICommandManager icm = server.getCommandManager();
-			ServerCommandManager scm = ((ServerCommandManager) icm);
-			scm.registerCommand(new CommandCPit());
-		}
+	@ServerStarting
+	public void serverStarting(FMLServerStartingEvent event) {
+		event.registerServerCommand(new CommandCPit());
 	}
 }
